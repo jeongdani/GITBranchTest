5b947c2aeafdd98efd5ba6bfae0f02daef2d5377
cherub1218 <only_1218@naver.com>
2015-01-30 17:12:56 +0900

    re

--- GitBranchTest/src/GitBranchTest3_dwjeong.java
+++ GitBranchTest/src/GitBranchTest3_dwjeong.java
@@ -3,6 +3,7 @@ public class GitBranchTest3_dwjeong {
 
 	public static void main(String[] args) {
 		System.out.println("GitBranchTest3_dwjeong");
+		System.out.println("GitBranchTest3_dhhan");
 
 	}
 
